ff77a1b4f2568efa4784781d2e0ca2837aaa6996
BuGalter <wardoc78@gmail.com>
2022-11-16 17:37:59 +0300

    feat: Generate cli to app resource account

---
+++ src/account/account.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAccountDto } from './dto/create-account.dto';
+
+@Injectable()
+export class AccountService {
+  create(createAccountDto: CreateAccountDto) {
+    return 'This action adds a new account';
+  }
+
+  findAll() {
+    return `This action returns all account`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} account`;
+  }
+}
