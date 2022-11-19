44cda23d6d9ba845648c4b4e5e7333543e11b669
BuGalter <wardoc78@gmail.com>
2022-11-19 18:52:25 +0300

    feat: Added to profile module dto for create profile

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProfileDto {}
+import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProfileDto {
+  @ApiProperty({ description: 'User name', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'User phone number', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @ApiProperty({ description: 'User address', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @ApiProperty({ description: 'Info about user', nullable: false })
+  @IsNotEmpty()
+  @IsString()
+  userInfo: string;
+}
