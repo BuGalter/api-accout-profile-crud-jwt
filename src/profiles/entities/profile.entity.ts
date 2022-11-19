e2cb0dbd22abd15fa54112940aa5d3f2ef8a21ff
BuGalter <wardoc78@gmail.com>
2022-11-19 18:51:46 +0300

    feat: Added profile entity to profile module

--- src/profiles/entities/profile.entity.ts
+++ src/profiles/entities/profile.entity.ts
@@ -1,1 +1,23 @@
-export class Profile {}
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Account } from '../../accounts/entities/account.entity';
+
+@Entity()
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  userInfo: string;
+
+  @OneToOne(() => Account, (account) => account.profile)
+  account: Account;
+}
