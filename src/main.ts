a4f044c6df5e5a3c6cbeb33e72ef1baeafc86966
BuGalter <wardoc78@gmail.com>
2022-11-19 18:50:46 +0300

    feat: Added to main file swagger config and global validate pipe for input data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .setTitle('API for user')
+    .setDescription('Test task')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
