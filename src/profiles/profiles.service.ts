8aaf397b5abdb4298215f056ed308e9a22d0804d
BuGalter <wardoc78@gmail.com>
2022-11-19 18:53:15 +0300

    change: Delete some methods and controllers in profile module

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Profile } from './entities/profile.entity';
 
 @Injectable()
 export class ProfilesService {
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+  ) {}
   create(createProfileDto: CreateProfileDto) {
     return 'This action adds a new profile';
   }
