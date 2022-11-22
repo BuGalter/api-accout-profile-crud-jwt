2434b81b68bbdd8f424fa13fddddeec4aa158db1
BuGalter <wardoc78@gmail.com>
2022-11-22 18:47:38 +0300

    feat: Added to swagger auth in main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setTitle('API for user')
     .setDescription('Test task')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
