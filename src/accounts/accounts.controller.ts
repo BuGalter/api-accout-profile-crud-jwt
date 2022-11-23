ef0ba0dad4627c50799a733f64c50344fce782af
BuGalter <wardoc78@gmail.com>
2022-11-23 11:05:30 +0300

    feat: Added to account module work logic
    feat: Added import auth module to account module
    feat: Added to controller endpoints
    feat: Added to services work logic with database and data from request

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { LoginAccountDto } from './dto/login-account.dto';
+import { Account } from './entities/account.entity';
 
-@Controller('accounts')
 @ApiTags('accounts')
+@Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
 
-  @Post()
-  create(@Body() createAccountDto: CreateAccountDto) {
+  @Post('account')
+  create(@Body() createAccountDto: CreateAccountDto): Promise<Account> {
     return this.accountsService.create(createAccountDto);
   }
 
@@ -19,13 +30,17 @@ export class AccountsController {
     return this.accountsService.login(loginAccountDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.accountsService.findOne(+id);
+  findOne(@Param('id') id: string, @Req() req: Request): Promise<any> {
+    return this.accountsService.findOne(+id, req);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get('logout/:id')
-  logout(@Param('id') id: string) {
-    return this.accountsService.logout(+id);
+  logout(@Param('id') id: string, @Req() req: Request): Promise<void> {
+    return this.accountsService.logout(+id, req);
   }
 }
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,30 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from 'express';
 import { Repository } from 'typeorm';
+import { AuthService } from '../auth/auth.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { LoginAccountDto } from './dto/login-account.dto';
 import { Account } from './entities/account.entity';
+import { EmailNotUniqueException } from './exceptions/email-not-unique.exception';
+import { AccountUnauthorizedException } from './exceptions/unauthorized.exception';
+import { UserNotFoundException } from './exceptions/user-not-found.exception';
 
 @Injectable()
 export class AccountsService {
   constructor(
     @InjectRepository(Account)
-    private accountRepository: Repository<Account>,
+    private readonly accountRepository: Repository<Account>,
+    private readonly authService: AuthService,
   ) {}
 
-  create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+  async create(createAccountDto: CreateAccountDto): Promise<Account> {
+    const { email, password } = createAccountDto;
+
+    const accountInDb = await this.accountRepository.findOneBy({ email });
+
+    if (accountInDb) {
+      throw new EmailNotUniqueException();
+    }
+
+    const account = new Account();
+    account.email = email;
+    account.password = password;
+
+    return await this.accountRepository.save(account);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
+  async findOne(id: number, req: Request): Promise<any> {
+    const idInToken = req.user;
+    // const account = await this.accountRepository.findOneBy({ id });
+
+    const account = await this.accountRepository.findOne({
+      where: {
+        id: 1,
+      },
+      relations: {
+        profile: true,
+      },
+    });
+
+    if (id !== +idInToken || !account.isLoggin) {
+      throw new UnauthorizedException();
+    }
+
+    if (!account) {
+      throw new UserNotFoundException();
+    }
+
+    const { password, isLoggin, profile, ...accountInfo } = account;
+
+    return {
+      accountInfo,
+      profile: account.profile,
+    };
   }
 
-  login(loginAccountDto: LoginAccountDto) {
-    return `login`;
+  async login(loginAccountDto: LoginAccountDto): Promise<any> {
+    const { email, password } = loginAccountDto;
+
+    const account = await this.accountRepository.findOneBy({ email });
+    if (!account) {
+      throw new UserNotFoundException();
+    }
+
+    const isValidPassword = await account.checkPassword(password);
+    if (!isValidPassword) {
+      throw new AccountUnauthorizedException();
+    }
+
+    await this.accountRepository.update(account.id, { isLoggin: true });
+
+    const payload = { id: account.id, email: account.email };
+
+    return await this.authService.createJwtToken(payload);
   }
 
-  logout(id: number) {
-    return `logout a #${id}`;
+  async logout(id: number, req: Request): Promise<void> {
+    const idInToken = req.user;
+    const account = await this.accountRepository.findOneBy({ id });
+
+    if (id !== +idInToken || !account.isLoggin) {
+      throw new UnauthorizedException();
+    }
+
+    if (!account) {
+      throw new UserNotFoundException();
+    }
+
+    await this.accountRepository.update(id, { isLoggin: false });
   }
 }
