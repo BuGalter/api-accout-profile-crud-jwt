b0ccb91f955bfc8ed2623b57a36d682467f8238e
BuGalter <wardoc78@gmail.com>
2022-12-01 20:04:19 +0300

    feat: Added import auth module to profile module

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -3,9 +3,11 @@ import { ProfilesService } from './profiles.service';
 import { ProfilesController } from './profiles.controller';
 import { Profile } from './entities/profile.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from '../accounts/entities/account.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile])],
+  imports: [TypeOrmModule.forFeature([Profile, Account]), AuthModule],
   controllers: [ProfilesController],
   providers: [ProfilesService],
 })
