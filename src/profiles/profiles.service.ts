be9e7031ae13d74f2a93f22c5797e8a9853c676b
BuGalter <wardoc78@gmail.com>
2022-12-01 20:07:31 +0300

    feat: Added to profile service all methods for processing profile controllers

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Account } from '../accounts/entities/account.entity';
+import { AuthService } from '../auth/auth.service';
+import { IAccountInfo } from '../interfaces/account-info.interface';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { Profile } from './entities/profile.entity';
@@ -9,21 +16,115 @@ import { Profile } from './entities/profile.entity';
 export class ProfilesService {
   constructor(
     @InjectRepository(Profile)
-    private profileRepository: Repository<Profile>,
+    private readonly profileRepository: Repository<Profile>,
+    @InjectRepository(Account)
+    private readonly accountRepository: Repository<Account>,
+    private readonly authService: AuthService,
   ) {}
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+
+  async create(
+    createProfileDto: CreateProfileDto,
+    req: any,
+  ): Promise<IAccountInfo> {
+    const id = req.user.accountId;
+
+    const { name, phoneNumber, address, userInfo } = createProfileDto;
+
+    const account = await this.accountRepository.findOneBy({ id });
+
+    await this.authService.validateAccount(account);
+    await this.authService.isAccountLoggin(account.isLoggin);
+
+    if (account.profile) {
+      throw new BadGatewayException();
+    }
+
+    const profile = new Profile();
+    profile.name = name;
+    profile.phoneNumber = phoneNumber;
+    profile.address = address;
+    profile.userInfo = userInfo;
+
+    await this.profileRepository.save(profile);
+
+    account.profile = profile;
+
+    await this.accountRepository.save(account);
+
+    const { password, isLoggin, ...accountInfo } = account;
+
+    return accountInfo;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  async findOne(accountId: number, req: any): Promise<IAccountInfo> {
+    const idInToken = req.user.accountId;
+
+    await this.authService.validateId(idInToken, accountId);
+
+    const account = await this.accountRepository.findOneBy({ id: accountId });
+
+    if (!account.profile) {
+      throw new NotFoundException();
+    }
+
+    await this.authService.isAccountLoggin(account.isLoggin);
+
+    const { password, isLoggin, ...accountInfo } = account;
+
+    return accountInfo;
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+  async update(
+    accountId: number,
+    updateProfileDto: UpdateProfileDto,
+    req: any,
+  ): Promise<IAccountInfo> {
+    const id = accountId;
+
+    const idInToken = req.user.accountId;
+
+    await this.authService.validateId(idInToken, id);
+
+    const account = await this.accountRepository.findOneBy({ id });
+
+    if (!account.profile) {
+      throw new NotFoundException();
+    }
+
+    await this.authService.isAccountLoggin(account.isLoggin);
+
+    const profile = Object.assign(account.profile, updateProfileDto);
+
+    await this.profileRepository.save(profile);
+
+    account.profile = profile;
+
+    await this.accountRepository.save(account);
+
+    const { password, isLoggin, ...accountInfo } = account;
+
+    return accountInfo;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+  async remove(accountId: number, req: any): Promise<any> {
+    const id = accountId;
+
+    const idInToken = req.user.accountId;
+
+    await this.authService.validateId(idInToken, id);
+
+    const account = await this.accountRepository.findOneBy({ id });
+
+    if (!account.profile) {
+      throw new NotFoundException();
+    }
+
+    await this.authService.isAccountLoggin(account.isLoggin);
+
+    const profileId = account.profile.id;
+
+    await this.profileRepository.delete({ id: profileId });
+
+    return { id: accountId };
   }
 }
